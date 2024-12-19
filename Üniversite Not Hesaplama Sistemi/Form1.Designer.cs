332beddbb7226a9b206435cf865ba6bd305871a1
crazy <crazy@SOFTWARE>
2024-12-19 21:18:32 +0300

    Add project files.

---
+++ Üniversite Not Hesaplama Sistemi/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Üniversite_Not_Hesaplama_Sistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.txtOrtalama = new System.Windows.Forms.Label();
+            this.txtHarfNotu = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(312, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vize Notu : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Final Notu : ";
+            // 
+            // txtVize
+            // 
+            this.txtVize.Location = new System.Drawing.Point(143, 43);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(140, 22);
+            this.txtVize.TabIndex = 2;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(143, 83);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(140, 22);
+            this.txtFinal.TabIndex = 2;
+            // 
+            // txtOrtalama
+            // 
+            this.txtOrtalama.AutoSize = true;
+            this.txtOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOrtalama.Location = new System.Drawing.Point(22, 118);
+            this.txtOrtalama.Name = "txtOrtalama";
+            this.txtOrtalama.Size = new System.Drawing.Size(64, 25);
+            this.txtOrtalama.TabIndex = 3;
+            this.txtOrtalama.Text = "label3";
+            // 
+            // txtHarfNotu
+            // 
+            this.txtHarfNotu.AutoSize = true;
+            this.txtHarfNotu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtHarfNotu.Location = new System.Drawing.Point(22, 153);
+            this.txtHarfNotu.Name = "txtHarfNotu";
+            this.txtHarfNotu.Size = new System.Drawing.Size(64, 25);
+            this.txtHarfNotu.TabIndex = 3;
+            this.txtHarfNotu.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 57);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "TEMİZLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtHarfNotu);
+            this.Controls.Add(this.txtOrtalama);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtVize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Not Hesaplama Sistemi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Label txtOrtalama;
+        private System.Windows.Forms.Label txtHarfNotu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Üniversite Not Hesaplama Sistemi/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Üniversite_Not_Hesaplama_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int vize = int.Parse(txtVize.Text);
+            int final = int.Parse(txtFinal.Text);
+
+            double ortalama = vize * 0.4 + final * 0.6;
+
+            txtOrtalama.Text = ortalama.ToString();
+
+            if (ortalama >= 87 && ortalama <= 100)
+            {
+                txtHarfNotu.Text= "AA";
+            }
+
+            if (ortalama >= 81 && ortalama <= 87)
+            {
+                txtHarfNotu.Text = "BA";
+            }
+
+            if (ortalama >= 74 && ortalama <= 80)
+            {
+                txtHarfNotu.Text = "BB";
+            }
+
+            if (ortalama >= 67 && ortalama <= 73)
+            {
+                txtHarfNotu.Text = "CB";
+            }
+
+            if (ortalama >= 60 && ortalama <= 66)
+            {
+                txtHarfNotu.Text = "CC";
+            }
+
+            if (ortalama >= 53 && ortalama <= 59)
+            {
+                txtHarfNotu.Text = "DC";
+            }
+
+            if (ortalama >= 46 && ortalama <= 52)
+            {
+                txtHarfNotu.Text = "DD";
+            }
+
+            if (ortalama >= 39 && ortalama <= 45)
+            {
+                txtHarfNotu.Text = "FD";
+            }
+
+            if (ortalama >= 0 && ortalama <= 38)
+            {
+                txtHarfNotu.Text = "FF";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtVize.Text = string.Empty;
+            txtFinal.Text = string.Empty;
+            txtOrtalama.Text = string.Empty;
+            txtHarfNotu.Text= string.Empty;
+        }
+    }
+}
